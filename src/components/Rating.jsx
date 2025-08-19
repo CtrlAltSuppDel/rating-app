23d2b7e468ed0652c4cb9a5cc601c7c7d2795d78
Daniel <danielandresogodieme@gmail.com>
2025-08-19 15:39:52 +0000

    feat: add modal component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Star from "./Star";
+import Modal from "./Modal";
 const Rating = ({ heading = "Rate your experience", color = "gold" }) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
@@ -46,19 +47,7 @@ const Rating = ({ heading = "Rate your experience", color = "gold" }) => {
       </button>
 
       {/* Modal */}
-      {submitted && (
-        <div className="modal-overlay">
-          <div className="modal">
-            <h2>Thank You ! </h2>
-            <p>
-              You rated us {rating} star{rating > 1 && "s"}
-            </p>
-            <button className="close-btn" onClick={closeModal}>
-              Close
-            </button>
-          </div>
-        </div>
-      )}
+      <Modal isOpenned={submitted} onClose={closeModal} rating={rating} />
     </div>
   );
 };
