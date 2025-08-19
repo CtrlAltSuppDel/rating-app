00de1e6e1457ef24cb53daf2655aae79f0b5c6e2
Daniel <danielandresogodieme@gmail.com>
2025-08-19 15:23:53 +0000

    feat: add submit button; add submit modal

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import Star from "./Star";
 const Rating = ({ heading = "Rate your experience", color = "gold" }) => {
-  const stars = Array.from({ length: 5 }, (_, id) => id + 1);
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
+  const stars = Array.from({ length: 5 }, (_, id) => id + 1);
   const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"];
+
+  const handleSubmit = () => {
+    if (rating > 0) {
+      setSubmitted(true);
+    }
+  };
+
+  const closeModal = () => {
+    setSubmitted(false);
+    setRating(0);
+    setHover(0);
+  };
+
   return (
     <div className="rating-container">
       <h2>{heading}</h2>
@@ -23,6 +37,28 @@ const Rating = ({ heading = "Rate your experience", color = "gold" }) => {
         ))}
       </div>
       {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p>}
+      <button
+        className="submit-btn"
+        onClick={handleSubmit}
+        disabled={rating === 0}
+      >
+        Submit
+      </button>
+
+      {/* Modal */}
+      {submitted && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>Thank You ! </h2>
+            <p>
+              You rated us {rating} star{rating > 1 && "s"}
+            </p>
+            <button className="close-btn" onClick={closeModal}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
