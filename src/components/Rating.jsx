1fd504da9d557f04f6015ffd2fdd51b169388ece
Daniel <danielandresogodieme@gmail.com>
2025-08-19 13:55:25 +0000

    feat: add conditional rendering & styling;

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 const Rating = () => {
   const stars = Array.from({length: 5}, (_, id) => id + 1)
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"]
   return (
   <div className="rating-container"> 
     <h2 >Rate Your Experience</h2>
     <div className="stars">
       {stars.map((star) => (
-        <span className="star" key={star}>{'\u2605'}</span>
+        <span 
+          onClick={ () => setRating(star)} 
+          onMouseEnter={ () => setHover(star)}
+          onMouseLeave={ () => setHover(0)}
+          key={star}
+          className = {`star ${star <= (hover || rating) ? 'active' : ''}`}
+        >{'\u2605'}</span>
       ))}
     </div>
+    {rating > 0 && <p className="feedback">{ feedbackMessages[rating-1] }</p>}
   </div>
   )
 }
