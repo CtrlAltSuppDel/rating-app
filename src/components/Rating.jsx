5fa6242c05118de7ec12c54a1b2306ad9d3e2474
Daniel <danielandresogodieme@gmail.com>
2025-08-19 14:41:41 +0000

    feat: adding properties to the rating component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-const Rating = () => {
+const Rating = ({heading = 'Rate your experience', color = 'gold'}) => {
   const stars = Array.from({length: 5}, (_, id) => id + 1)
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
   const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"]
   return (
   <div className="rating-container"> 
-    <h2 >Rate Your Experience</h2>
+    <h2 >{heading}</h2>
     <div className="stars">
       {stars.map((star) => (
         <span 
@@ -15,7 +15,10 @@ const Rating = () => {
           onMouseEnter={ () => setHover(star)}
           onMouseLeave={ () => setHover(0)}
           key={star}
-          className = {`star ${star <= (hover || rating) ? 'active' : ''}`}
+          className = 'star'
+          style={
+            {color: `${star <= (hover || rating) ? color : '#ccc'}`}
+          }
         >{'\u2605'}</span>
       ))}
     </div>
